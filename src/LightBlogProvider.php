52f6083f03c7773436e7282141f431256f7931e6
Pavin Disatapundhu <disatapp@gmail.com>
2019-07-06 11:49:41 +0700

    master

--- src/LightBlogProvider.php
+++ src/LightBlogProvider.php
@@ -14,7 +14,7 @@ class LightBlogProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        // $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'lightBlog');
         $this->publishes([
             __DIR__ . '/config' => config_path('light-blog-demo'),
